2da8e7ddd6a8b510b99e4def859726b47830f269
natz92 <natz.galaxy@gmail.com>
2021-03-24 07:54:38 +0900

    yaml 사용

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/natz92/m3u8
 
 go 1.16
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -3,26 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/natz92/m3u8/dl"
+	"gopkg.in/yaml.v2"
 )
 
 var (
 	url            string
+	yamlFile       string
 	output         string
 	outputFileName string
 	chanSize       int
 )
 
+type FileInfo struct {
+	Url        string `yaml:"url"`
+	Id         string `yaml:"id"`
+	Name       string `yaml:"name"`
+	Downloaded bool   `yaml:downloaded`
+}
+
+type FileInfos struct {
+	Infos []FileInfo `yaml:"files"`
+}
+
 func init() {
 	flag.StringVar(&url, "u", "", "M3U8 URL, required")
+	flag.StringVar(&yamlFile, "i", "", "M3U8 yaml info, required")
 	flag.IntVar(&chanSize, "c", 25, "Maximum number of occurrences")
 	flag.StringVar(&output, "o", "", "Output folder, required")
 	flag.StringVar(&outputFileName, "f", "", "Output File Name")
 }
 
 func main() {
+	f := FileInfos{}
+
 	flag.Parse()
 	defer func() {
 		if r := recover(); r != nil {
@@ -30,11 +47,8 @@ func main() {
 			os.Exit(-1)
 		}
 	}()
-	if url == "" {
-		panicParameter("u")
-	}
-	if output == "" {
-		panicParameter("o")
+	if yamlFile == "" {
+		panicParameter("i")
 	}
 	if outputFileName == "" {
 		outputFileName = "merged"
@@ -42,14 +56,36 @@ func main() {
 	if chanSize <= 0 {
 		panic("parameter 'c' must be greater than 0")
 	}
-	downloader, err := dl.NewTask(output, outputFileName, url)
+
+	yamlFile, err := ioutil.ReadFile(yamlFile)
 	if err != nil {
 		panic(err)
 	}
-	if err := downloader.Start(chanSize); err != nil {
-		panic(err)
+	// fmt.Printf("--- s\n:%s", yamlFile)
+	err = yaml.Unmarshal(yamlFile, &f)
+	if err != nil {
+		fmt.Printf("Unmarshal: %s", err)
 	}
-	fmt.Println("Done!")
+	// fmt.Printf("--- t:\n%v\n\n", f)
+
+	for i, v := range f.Infos {
+		if v.Downloaded == true {
+			continue
+		}
+		fmt.Printf("%d, %s: Start.", i, v.Id)
+
+		downloader, err := dl.NewTask("output", v.Name, v.Url)
+		if err != nil {
+			panic(err)
+		}
+		if err := downloader.Start(chanSize); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%d, %s: Done.", i, v.Id)
+	}
+
+	fmt.Println("\n\n All Done!")
 }
 
 func panicParameter(name string) {
